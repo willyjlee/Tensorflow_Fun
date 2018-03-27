664d2efc457584ab9bfae36e870637beef73f775
willyjlee <williamjlee@ucla.edu>
2018-03-26 22:21:48 -0700

    fix GAN bugs

--- GANs/mnist_gan.py
+++ GANs/mnist_gan.py
@@ -12,9 +12,9 @@ class generator:
     def __init__(self):
         self.l1_size = 100
         self.w1 = tf.Variable(tf.truncated_normal(shape=[noise_size, self.l1_size], stddev=np.sqrt(2.0 / (noise_size))))
-        self.b1 = tf.zeros([self.l1_size], tf.float32)
+        self.b1 = tf.Variable(tf.zeros([self.l1_size], tf.float32))
         self.w2 = tf.Variable(tf.truncated_normal(shape=[self.l1_size, 784], stddev=np.sqrt(2.0 / (self.l1_size))))
-        self.b2 = tf.zeros([784], tf.float32)
+        self.b2 = tf.Variable(tf.zeros([784], tf.float32))
 
     def outputs(self, z_holder):
         l1 = tf.nn.relu(tf.matmul(z_holder, self.w1) + self.b1)
@@ -25,9 +25,9 @@ class discriminator:
     def __init__(self):
         self.l1_size = 200
         self.w1 = tf.Variable(tf.truncated_normal(shape=[784, self.l1_size], stddev=np.sqrt(2.0 / (784))))
-        self.b1 = tf.zeros([self.l1_size], tf.float32)
+        self.b1 = tf.Variable(tf.zeros([self.l1_size], tf.float32))
         self.w2 = tf.Variable(tf.truncated_normal(shape=[self.l1_size, 1], stddev=np.sqrt(2.0 / self.l1_size)))
-        self.b2 = tf.zeros([1], tf.float32)
+        self.b2 = tf.Variable(tf.zeros([1], tf.float32))
 
     def outputs(self, x_holder):
         l1 = tf.nn.relu(tf.matmul(x_holder, self.w1) + self.b1)
@@ -44,14 +44,15 @@ def setup():
     dis_loss = -tf.reduce_mean(tf.log(dis_out) + tf.log(1.0 - dis_gen_out))
     gen_loss = -tf.reduce_mean(tf.log(dis_gen_out))
 
-    dis_opt = tf.train.AdamOptimizer(1e-4).minimize(dis_loss)
-    gen_opt = tf.train.AdamOptimizer(1e-4).minimize(gen_loss)
+    dis_opt = tf.train.AdamOptimizer(1e-4).minimize(dis_loss, var_list=[dis.w1, dis.b1, dis.w2, dis.b2])
+    gen_opt = tf.train.AdamOptimizer(1e-4).minimize(gen_loss, var_list=[gen.w1, gen.b1, gen.w2, gen.b2])
     return (dis_opt, dis_loss, gen_opt, gen_loss, gen_out), z_holder, x_holder
 
 def disp(img):
     plt.imshow(img.reshape(28, 28), cmap='Greys_r')
     plt.show()
 
+%matplotlib inline
 def train_loop():
     mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
     runs, z_holder, x_holder = setup()
@@ -66,6 +67,13 @@ def train_loop():
             z_rand = np.random.uniform(-1.0, 1.0, size=[batch_size, noise_size])
             _, gen_loss, gen_out = sess.run(runs[2:], feed_dict={z_holder: z_rand})
 
+            if i % 100 == 0:
+                pass
+#                 print("disloss: {}".format(dis_loss))
+#                 print("genloss: {}".format(gen_loss))
+            if i % 500 == 0:
+                pass
+#                 print(gen_out)
             if i % 2500 == 0:
                 print("Iter: {}".format(i))
                 print("disloss: {}".format(dis_loss))
